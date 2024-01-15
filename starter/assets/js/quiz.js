141c574c44dc748a0aca8125302a948dfef6649d
Harry <harry@macbookpro>
2024-01-15 19:46:17 +0000

    created js file with questions array

---
+++ starter/assets/js/quiz.js
@@ -0,0 +1,28 @@
+const TotalQuestions = [
+  {
+    question: "What is the capital of France?",
+    answers: ["Berlin", "Paris", "Madrid", "Rome"],
+    correctIndex: 1,
+  },
+  {
+    question:
+      "Which programming language is this code written in? \n\n```python\nprint('Hello, World!')```",
+    answers: ["Java", "Python", "C++", "JavaScript"],
+    correctIndex: 1,
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    answers: ["Venus", "Jupiter", "Mars", "Saturn"],
+    correctIndex: 1,
+  },
+  {
+    question: "Which of the following is not a primary color?",
+    answers: ["Red", "Blue", "Yellow", "Green"],
+    correctIndex: 3,
+  },
+  {
+    question: "In what year did Christopher Columbus reach the Americas?",
+    answers: ["1492", "1607", "1776", "1812"],
+    correctIndex: 0,
+  },
+];
